2f40acb5d3d94ed41326d04a0ce59a8d2e347f81
Andrew Farabow <aafarabow@gmail.com>
2020-01-26 20:07:37 -0500

    more library progress

--- policy_gradient.py
+++ policy_gradient.py
@@ -1,75 +1,49 @@
 import torch
 from collections import deque
-import statistics
-from visualize import update_viz
 
 
+def step_environment(state, policy, env, trajectories, episode_reward):
+    action = policy(torch.tensor(state, dtype=torch.float32))
+    new_state, reward, done, _ = env.step(action.item())
 
+    episode_reward += reward
 
+    trajectories.append({
+        "state": state,
+        "action": action,
+        "reward": torch.tensor([reward]),
+        "done": done
+    })
 
-# %%
-episode_reward = 0
-last_episode_rewards = []
+    return new_state, episode_reward
 
+def improve(policy, policy_optimizer, env, trajectories, episode_reward):
+    states = torch.tensor([trajectory["state"] for trajectory in trajectories], dtype=torch.float32)
+    actions = torch.tensor([trajectory["action"] for trajectory in trajectories], dtype=torch.float32)
+    dones = torch.tensor([torch.tensor([1.0]) if trajectory["done"] else torch.tensor([0.0]) for trajectory in trajectories], dtype=torch.float32)
+    rewards = [trajectory["reward"] for trajectory in trajectories]
 
-# %%
-for episode in range(100000):
-    trajectories = []
-
-    state = env.reset()
-
-    if len(last_episode_rewards) == 100:
-        avg_ep_reward = statistics.mean(last_episode_rewards)
-        print(avg_ep_reward)
-        update_viz(episode, avg_ep_reward)
-        last_episode_rewards.clear()
-
-    while len(trajectories) == 0 or not trajectories[-1]["done"]:
-        action = policy(torch.tensor(state, dtype=torch.float32))
-        new_state, reward, done, _ = env.step(action.item())
-
-        episode_reward += reward
-
-        trajectories.append({
-            "state": state,
-            "action": action,
-            "reward": torch.tensor([reward]),
-            "done": done
-        })
-
-        state = new_state
-
-    last_episode_rewards.append(episode_reward)
-    episode_reward = 0
-
-    if episode % 10 == 0:
-
-        states = torch.tensor([trajectory["state"] for trajectory in trajectories], dtype=torch.float32)
-        actions = torch.tensor([trajectory["action"] for trajectory in trajectories], dtype=torch.float32)
-        dones = torch.tensor([torch.tensor([1.0]) if trajectory["done"] else torch.tensor([0.0]) for trajectory in trajectories], dtype=torch.float32)
-        rewards = [trajectory["reward"] for trajectory in trajectories]
-
-        returns = [0] * len(rewards)
-        discounted_future = 0
+    returns = [0] * len(rewards)
+    discounted_future = 0
+    
+    for i in reversed(range(len(rewards))):
+        if dones[i]:
+            returns[i] = rewards[i]
+        else:
+            returns[i] = rewards[i] + discounted_future
         
-        for i in reversed(range(len(rewards))):
-            if dones[i]:
-                returns[i] = rewards[i]
-            else:
-                returns[i] = rewards[i] + discounted_future
-            
-            discounted_future = returns[i] * 0.99
+        discounted_future = returns[i] * 0.99
 
-        returns = torch.tensor(returns)
+    returns = torch.tensor(returns)
 
-        mean = returns.mean()
-        std = returns.std() + 1e-6
-        returns = (returns - mean)/std
-        
-        log_probs = policy.log_prob(states, actions)
+    mean = returns.mean()
+    std = returns.std() + 1e-6
+    returns = (returns - mean)/std
+    
+    log_probs = policy.log_prob(states, actions)
 
-        policy_loss = -(torch.dot(returns, log_probs)).mean()
+    policy_loss = -(returns * log_probs).mean()
 
-        policy_optimizer.zero_grad()
-        policy_loss.backward()
-        policy_optimizer.step()
+    policy_optimizer.zero_grad()
+    policy_loss.backward()
+    policy_optimizer.step()
